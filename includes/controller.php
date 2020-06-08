5c5bacf46e170805ab8d0183a813557d9be49339
Luis Miranda <luismiranda1995@gmail.com>
2020-06-07 17:06:52 -0700

    added calendar plugin on home pg that loads first event in DB

--- includes/controller.php
+++ includes/controller.php
@@ -4,9 +4,20 @@ include 'databaseAdaptor.php';
 
 $calendarDB = new DatabaseAdaptor();
 
-// An array containing each row in the database.
-// Each row is an object that represents an 
-// individual event.
-$events = $calendarDB->getEvents();
-echo json_encode($events[0]);    
 
+// If currently on the homepage, load the single next
+// event into the "calendar-container" on index.html
+if (isset($_GET['onHomePage'])){
+
+    // An array containing each row in the database.
+    // Each row is an object that represents an 
+    // individual event.
+    $events = $calendarDB->getEvents();
+
+    if (count($events) == 0){
+        echo "No upcoming events.";
+    } else {
+        // send only the first events
+        echo json_encode($events[0]);  
+    } 
+}

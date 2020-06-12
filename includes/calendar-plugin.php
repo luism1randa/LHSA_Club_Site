30d8bda72cd2e49065498e671df2edb9e982f221
Luis Miranda <luismiranda1995@gmail.com>
2020-06-12 15:05:47 -0700

    created a dateTime parser for event details

--- includes/calendar-plugin.php
+++ includes/calendar-plugin.php
@@ -30,37 +30,98 @@ $options = array(
     'timeMin' => $minStartDate,
 );
 
-// Results format:
-// array['items'][i]['property']
+// Results format: object['items'][i]['property']
 $results = $service->events->listEvents($calendarId, $options);
 
-// TODO: check base case of no upcoming events
-
-// TODO: add queries for single event and multiple events
-
 // Get the the next upcoming event
-$nextEvent = array(
-    'summary' => $results['items'][0]['summary'],
-    'start' => $results['items'][0]['start']['dateTime'],
-    'end' => $results['items'][0]['end']['dateTime'],
-    'location' => $results['items'][0]['location'],
-    'description' => $results['items'][0]['description'],
-);
+if(isset($_GET['nextEvent'])){
 
-$eventList = array();
+    if (count($results['items']) == 0){
+        echo 'No upcoming events.';
+    } else {
 
-// Get all upcoming events
-for ($i=0; $i < count($results['items']); $i++) { 
-    $event = array(
-        'summary' => $results['items'][$i]['summary'],
-        'start' => $results['items'][$i]['start']['dateTime'],
-        'end' => $results['items'][$i]['end']['dateTime'],
-        'location' => $results['items'][$i]['location'],
-        'description' => $results['items'][$i]['description'],
+        $start = $results['items'][0]['start']['dateTime'];
+        $end = $results['items'][0]['end']['dateTime'];
+        $dateTime = parseDateTime($start, $end);
+
+        $nextEvent = array(
+            'summary' => $results['items'][0]['summary'],
+            'month' => $dateTime['month'],
+            'day' => $dateTime['day'],
+            'start' => $dateTime['startTime'],
+            'end' => $datTime['endTime'],
+            'location' => $results['items'][0]['location'],
+            'description' => $results['items'][0]['description'],
+        );
+    }   
+}
+
+if (isset($_GET['allNextEvents'])){
+    
+    $eventList = array();
+    for ($i=0; $i < count($results['items']); $i++) { 
+        $event = array(
+            'summary' => $results['items'][$i]['summary'],
+            'start' => $results['items'][$i]['start']['dateTime'],
+            'end' => $results['items'][$i]['end']['dateTime'],
+            'location' => $results['items'][$i]['location'],
+            'description' => $results['items'][$i]['description'],
+        );
+        $eventList[$i] = $event;
+    };
+}
+
+function parseDateTime($start, $end){
+    /**
+     * This is a helper function that parses a dateTime in standard format
+     * to extract the start time, end time, month, and day.
+     * 
+     * dateTime format: 2020-06-12T14:30:00-07:00
+     * 
+     * @param string $start a dateTime input for the start of the event
+     * @param string $end a dateTime input for the end of the event
+     * 
+     * @return array an array containing all the details of the event
+     */
+
+
+    // extract start time
+    $startHour = substr($start, 11, 2);
+    $startMin = substr($start, 14, 2);
+
+    // convert military start time to standard time
+    if ($startHour > 12) {
+        $startHour -= 12;
+        $startTime = $startHour . ':' . $startMin . 'PM';
+    } else {
+        $startTime = $startHour . ':' . $startMin . 'AM';
+    }
+
+    // extract end time
+    $endHour = substr($end, 11, 2);
+    $endMin = substr($end, 14, 2);
+
+    // convert military end time to standard time
+    if ($endHour > 12) {
+        $endHour -= 12;
+        $endTime = $endHour . ':' . $endMin . 'PM';
+    } else {
+        $endTime = $endHour . ':' . $endMin . 'AM';
+    }
+
+    // Convert month digit to abbreviation
+    $month = substr($start, 5, 2) - 1;
+    $monthList = 'JanFebMarAprMayJunJulAugSepOctNovDec';
+    $monthAbbrev = substr($monthList, $month * 3, 3);
+
+    // extract two digit date
+    $day = substr($start, 8, 2);
+
+    return array (
+        'startTime' => $startTime,
+        'endTime' => $endTime,
+        'month' => $monthAbbrev,
+        'day' => $day,
     );
-    $eventList[$i] = $event;
-};
-
-print_r($nextEvent);
-
+}
 ?>
--- includes/controller.php
+++ includes/controller.php
@@ -4,19 +4,3 @@ include 'databaseAdaptor.php';
 
 $calendarDB = new DatabaseAdaptor();
 
-// If currently on the homepage, load the single next
-// event into the "calendar-container" on index.html
-if (isset($_GET['onHomePage'])){
-
-    // An array containing each row in the database.
-    // Each row is an object that represents an 
-    // individual event.
-    $events = $calendarDB->getEvents();
-
-    if (count($events) == 0){
-        echo "No upcoming events.";
-    } else {
-        // send only the first events
-        echo json_encode($events[0]);  
-    } 
-}
